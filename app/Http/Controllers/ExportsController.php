dba8e644f1d288d0ca1718e761063ab70d3b4a10
Paulonbass75 <paulmpool@yahoo.com>
2024-01-04 15:49:08 -0500

    first commit

---
+++ app/Http/Controllers/ExportsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Maatwebsite\Excel\Facades\Excel;
+
+use App\Exports\UsersExport;
+use App\Imports\OrdersImport;
+use App\Imports\UserImport;
+use App\Models\Order;
+use App\Models\User;
+
+class ExportsController extends Controller
+{
+    public function export()
+    {
+        $users = User::all();
+        $orders = Order::all();
+        // return Excel::store(new UsersExport($users), 'users.xlsx');
+        (new UsersExport($users))->store('users.xlsx')->chain([
+            // jobs to execute
+        ]);
+        return 'The Export has Started';
+        // return (new UsersExport($users));
+    }
+
+    // public function import()
+    // {
+    //     // Excel::import(new UserImport, 'Users_Import_1.xlsx');
+    //     // (new UserImport())->import('Users_Import_1.xlsx');
+    //     (new OrdersImport())->import('Users_Import_2.xlsx');
+    //     return '1';
+    // }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\OrderItem;
+use App\Models\Restaurant;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // 10 Restaurants. Each Restaurant may have menu items ranging between 5 and 30
+        Restaurant::factory(10)->hasMenuItems(rand(5, 30))->create();
+        $this->command->info('Restaurant Seeder Completed');
+
+        // 100 Users. Each User may have more than 1 orders.
+        User::factory(100)->hasOrders(rand(1,100))->create();
+        $this->command->info('User Seeder Completed');
+
+        // Order Items
+        OrderItem::factory(200)->create();
+        $this->command->info('Order Items Seeder Completed');
+    }
+}
